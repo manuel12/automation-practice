16ba9c4f2bdaba086009929b7476cbbf2aad7199
Manuel <manuelpinedacabeza@gmail.com>
2024-05-15 13:41:53 +0200

    Added JSDocs and changed timestamp to use 6 digits.

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,6 +1,12 @@
-export const generateUniqueEmail = (email) => {
-    const timestamp = new Date().getTime() % 10000 // Ensure timestamp is at most 4 digits
-    const [username, domain] = email.split("@")
-    const uniqueEmail = `${username}+${timestamp}@${domain}`
-    return uniqueEmail
+/**
+ * Generates a unique email address based on the current timestamp.
+ * @returns {string} The generated unique email address.
+ */
+
+export const generateUniqueEmail = () => {
+  const email = "testuser@gmail.com"
+  const timestamp = new Date().getTime() % 1000000 // Ensure timestamp is at most 6 digits
+  const [username, domain] = email.split("@")
+  const uniqueEmail = `${username}_${timestamp}@${domain}`
+  return uniqueEmail
 }
