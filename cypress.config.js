1871f35657825cc9937aa2d1633838d31374bd7c
Manuel <manuelpinedacabeza@gmail.com>
2024-11-19 12:29:14 +0100

    Added viewport H&W removed watch for file changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-});
+    specPattern: "cypress/e2e/automation-practice/**/*.{js,jsx,ts,tsx}",
+
+    viewportWidth: 1200,
+    viewportHeight: 1000,
+    watchForFileChanges: false
+  }
+})
