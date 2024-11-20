5eb28038d4f4ed1420d6c4d101dc4ec1a06dcd8f
Manuel <manuelpinedacabeza@gmail.com>
2024-11-20 12:13:17 +0100

    Added checkout shopping cart dropdowwn tests

---
+++ cypress/e2e/automation-practice/shopping-cart-dropdown-list.cy.js
@@ -0,0 +1,228 @@
+/// <reference types="cypress" />
+
+const userCredentials = require("../../fixtures/user-credentials.json")
+const {
+  printedSummerDressInOrange,
+  printedSummerDressInSkyBlue
+} = require("../../fixtures/products.json")
+
+describe("Shopping Cart - Dropdown List", () => {
+  context(
+    "As a user I want a shopping cart on every page of the catalogue so that I can keep track of my products:",
+    () => {
+      beforeEach(() => {
+        cy.visit("http://www.automationpractice.pl/")
+
+        cy.loginUser(userCredentials)
+      })
+
+      it("should display shopping cart element in all pages of the catalogue", () => {
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get("#block_top_menu").contains("Women").click()
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get(".sf-menu > :nth-child(2) > .sf-with-ul").click()
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get(".sf-menu > :nth-child(3) > a").click()
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get(".nav").contains("Contact us").click()
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get(".nav").contains("Test user").click()
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+      })
+    }
+  )
+
+  context(
+    "As a user I want to see a dropdown list of all my products by hovering over shopping cart so that I can keep track of my products:",
+    () => {
+      beforeEach(() => {
+        cy.visit("http://www.automationpractice.pl/")
+
+        cy.loginUser(userCredentials)
+
+        cy.addProductToCart(printedSummerDressInOrange)
+
+        cy.addProductToCart(printedSummerDressInSkyBlue)
+      })
+
+      it("should display shopping cart dropdown product list when hovering over shopping cart element", () => {
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get('[title="View my shopping cart"]').trigger("mouseover")
+
+        cy.get("dl.products").should("be.visible")
+      })
+    }
+  )
+
+  context(
+    "As a user I want the products in my shopping cart to display their name, quantity, size, color, and price so that I can keep track of their features:",
+    () => {
+      beforeEach(() => {
+        cy.visit("http://www.automationpractice.pl/")
+
+        cy.loginUser(userCredentials)
+
+        cy.addProductToCart(printedSummerDressInOrange)
+
+        cy.addProductToCart(printedSummerDressInSkyBlue)
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get('[title="View my shopping cart"]').trigger("mouseover")
+
+        cy.get("dl.products").should("be.visible")
+      })
+
+      it("should display all products added to the shopping cart in dropdown list including name, quantity, size, color and price", () => {
+        cy.get('[data-id^="cart_block_product_"]')
+          .should("be.visible")
+          .first()
+          .within(() => {
+            cy.get(".cart-images > img").should("be.visible")
+
+            cy.get(".cart-info")
+              .should("be.visible")
+              .within(() => {
+                cy.get(".product-name").should("be.visible")
+
+                cy.get(".product-atributes").should("be.visible")
+
+                cy.get(".price").should("be.visible")
+              })
+          })
+
+        cy.get('[data-id^="cart_block_product_"]')
+          .should("be.visible")
+          .eq(1)
+          .within(() => {
+            cy.get(".cart-images > img").should("be.visible")
+
+            cy.get(".cart-info")
+              .should("be.visible")
+              .within(() => {
+                cy.get(".product-name").should("be.visible")
+
+                cy.get(".product-atributes").should("be.visible")
+
+                cy.get(".price").should("be.visible")
+              })
+          })
+      })
+    }
+  )
+
+  context(
+    "As a user I want to be able to remove products directly from the shopping cart list so that I can take out any products added incorrectly:",
+    () => {
+      beforeEach(() => {
+        cy.visit("http://www.automationpractice.pl/")
+
+        cy.loginUser(userCredentials)
+
+        cy.addProductToCart(printedSummerDressInOrange)
+
+        cy.addProductToCart(printedSummerDressInSkyBlue)
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get('[title="View my shopping cart"]').trigger("mouseover")
+
+        cy.get("dl.products").should("be.visible")
+      })
+
+      it("should allow user to remove any products added to the shopping cart", () => {
+        cy.get('[data-id^="cart_block_product_"]').should("have.length", 2)
+
+        cy.get('[data-id^="cart_block_product_"]')
+          .should("be.visible")
+          .first()
+          .within(() => {
+            cy.get(".remove_link").should("be.visible").click()
+          })
+          .then(() => {
+            cy.get('[data-id^="cart_block_product_"]').should("have.length", 1)
+
+            cy.get('[data-id^="cart_block_product_"]')
+              .should("be.visible")
+              .first(1)
+              .within(() => {
+                cy.get(".remove_link").should("be.visible").click()
+              })
+              .then(() => {
+                cy.get('[data-id^="cart_block_product_"]').should(
+                  "have.length",
+                  0
+                )
+
+                cy.get('[title="View my shopping cart"]')
+                  .should("be.visible")
+                  .and("include.text", "Cart", "(empty)")
+              })
+          })
+      })
+    }
+  )
+
+  context(
+    "As a user I want to the price of shipping to be displayed in the shopping cart so that I don't get unexpected charges:",
+    () => {
+      beforeEach(() => {
+        cy.visit("http://www.automationpractice.pl/")
+
+        cy.loginUser(userCredentials)
+
+        cy.addProductToCart(printedSummerDressInOrange)
+
+        cy.addProductToCart(printedSummerDressInSkyBlue)
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get('[title="View my shopping cart"]').trigger("mouseover")
+
+        cy.get("dl.products").should("be.visible")
+      })
+
+      it("should display the price of shipping in the shopping cart", () => {
+        cy.get(".cart_block_shipping_cost")
+          .should("be.visible")
+          .and("have.text", "$7")
+      })
+    }
+  )
+
+  context(
+    "As a user I want to see the total price of my products + shipping in the shopping cart so that I don't get unexpected charges:",
+    () => {
+      beforeEach(() => {
+        cy.visit("http://www.automationpractice.pl/")
+
+        cy.loginUser(userCredentials)
+
+        cy.addProductToCart(printedSummerDressInOrange)
+
+        cy.addProductToCart(printedSummerDressInSkyBlue)
+
+        cy.get('[title="View my shopping cart"]').should("be.visible")
+
+        cy.get('[title="View my shopping cart"]').trigger("mouseover")
+
+        cy.get("dl.products").should("be.visible")
+      })
+
+      it.only("should display the total price of products + shipping in the shopping cart", () => {
+        cy.get(".cart_block_total").should("be.visible").and("have.text", "$36")
+      })
+    }
+  )
+})
